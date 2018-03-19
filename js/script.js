4b6177e8fb343877146ff569e3f1c13c5730ad91
cobaltsync <server@cobalt-mc.com>
2018-03-19 02:14:05 +0100

    First Upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+
+var currentPage = "#findus-page";
+
+// Changes to another page smoothly.
+function transitionToPage(toPage){
+	if(currentPage == toPage)
+		return;	
+	$(toPage).css("display", "grid");
+	$(toPage).finish().animate({opacity: '1'}, 200);
+	$(currentPage).finish().animate({opacity: '0'}, 200, function() {
+		$(currentPage).css("display", "none");
+		currentPage = toPage;
+	});
+}
+
+// Opens & closes the sidebar.
+function toggleSidebar(){
+	$(".navbar-mobile aside").toggleClass("active");
+	$("#sidebar-button").toggleClass("active");
+	$("main").toggleClass("active");
+}
+
+// Empty prompt.
+function sessionEditTime() {
+	prompt("Change time and date of the training to?", "Y-M-D | H:M");
+};
+
+// Fake logout alert, just reloads the page.
+function logout() {
+	alert("You have logged out!");
+	location.reload();
+};
+
+// Can't have a onclick="deleteSession()" in html because I need 
+// a reference to the clicked element so i can delete it's parent.
+$(document).on("click", ".session div #red", function() {
+	if(confirm("Are you sure you want to cancel this training session?"))
+		$(this).parent().parent().remove(); 
+});
